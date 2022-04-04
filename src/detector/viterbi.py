952cd8736f21fdb675492ec847ec6c90d6d15ad3
Guillaume Joyet <guillaume.joyet@unibas.ch>
2022-04-04 10:06:20 +0200

    Finish implementing viterbi algorithm. Tested with dummy parameters, true transmission and emission parameters are still missing.

--- src/detector/viterbi.py
+++ src/detector/viterbi.py
@@ -1,39 +1,49 @@
 import numpy as np
 
-to_index = {'A': 0, 'C': 1, 'G': 2, 'T': 3,
-            'A+': 0, 'C+': 1, 'G+': 2, 'T+': 3,
+to_index = {'A+': 0, 'C+': 1, 'G+': 2, 'T+': 3,
             'A-': 4, 'C-': 5, 'G-': 6, 'T-': 7}
 to_state = {0: 'A+', 1: 'C+', 2: 'G+', 3: 'T+',
             4: 'A-', 5: 'C-', 6: 'G-', 7: 'T-'}
 # Likelihood of transition from (row) to (column).
 # Order: A+, C+, G+, T+, A-, C-, G-, T-.
-trans_cpgi = np.array()
-trans_non_cpgi = np.array()
-
+trans = np.array()
 # Emission probabilities.
 # Order: A+, C+, G+, T+, A-, C-, G-, T-.
-emission_cpgi = np.array()
-emission_non_cpgi = np.array()
+emission = np.array()
 
 
-def viterbi(sequence: str) -> list[str]:
+def viterbi(sequence: str) -> list[list[str]]:
     # This is wrong: I need the transition / emission matrices AND
     # need to keep at least two columns of v at a time.
 
-    v_prev = 1.0
+    n = len(sequence)
+    char = sequence[0]
+    v = np.zeros((8, n))
     ptr = ['S']
+    v[to_index[char + '+'], 0] = emission[to_index[char + '+']] / 8.0
+    v[to_index[char + '-'], 0] = emission[to_index[char + '-']] / 8.0
 
-    for i in range(len(sequence)):
-        v = np.zeros(8)
+    for i in range(1, n):
         char = sequence[i]
-        cpgi = char + '+'
-        no_cpgi = char + '-'
-        v[to_index[cpgi]] = emission_cpgi[to_index[cpgi]] * v_prev * \
-                                  trans_cpgi[to_index[ptr[-1]], to_index[char]]
-        v[to_index[no_cpgi]] = emission_non_cpgi[to_index[no_cpgi]] * v_prev * \
-                                  trans_non_cpgi[to_index[ptr[-1]], to_index[char]]
-        v_prev = np.max(v)
-        ptr.append(to_state[np.argmax(v)])
+        v[to_index[char + '+'], i] = emission[to_index[char + '+']] * np.max(v[:, i-1] * trans[:, to_index[char + '+']])
+        v[to_index[char + '-'], i] = emission[to_index[char + '-']] * np.max(v[:, i-1] * trans[:, to_index[char + '-']])
+        max_state = np.argmax(v[:, i])
+        ptr.append(to_state[np.argmax(v[:, i - 1] * trans[:, max_state])])
 
+    ptr.append(to_state[np.argmax(v[:, -1])])
     ptr.append('E')
     return ptr
+
+
+if __name__ == '__main__':
+    seq = 'GGAACCAAGAACGAGGGGCAAGTGGGAGGAGGTGGTCACCTGGAGGG' \
+          'TGTGGACCAGTGGTACACAGGTTAGGAGAGGGGGAAGGGCAGAGTTT' \
+          'ACATTGCCCGTATGCTGGCGAGTGAAGTCCACTAGGAACTGAGACAT' \
+          'GAACTTGAGGCTTAGCAAAAGAGAGCGACTTAGAGAAAGAGCACCCG' \
+          'CACTGGTGACTGTGGGCTGCATGGTGAAGGGGGGCAAAGCAGTGACA' \
+          'GCGGGAGTGAGCCCCTCTCAAAAACTGATGCCAACTACGCAGGACAG' \
+          'AGAGGGGGCGGGGAAGGGGGAGTGACCTGAGGGAGACTGGGGCTCAA' \
+          'GAAAAGCCTTTTTGTGTTGGTTGTTTTAAAGGCTGGCGATACTGTAG' \
+          'CATGCTTAGTTCTAAGGAGAGGAA'
+    ptr = viterbi(seq)
+    print(ptr)
