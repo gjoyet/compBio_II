ef9dd8e0e8aaa22ce9e6112893d6111268c1fdad
Guillaume Joyet <guillaume.joyet@unibas.ch>
2022-04-07 21:16:45 +0200

    Forward and backward calculations seem correct (at least they yield the same result for p(x)). Update of transitions to end state are not yet done, leading them to be all 0 starting from the second iteration. Will have to ask Zavolan how we should do that.

--- src/detector/baum_welch.py
+++ src/detector/baum_welch.py
@@ -22,38 +22,73 @@ class BaumWelch:
         # sequences
         self.seqs = seqs
 
-    def run(self, seqs: list[str]):
-        pass
+    def run(self):
+        for i in range(5):
+            self.iterate()
+
+    def iterate(self):
+        all_f = []
+        all_p = []
+        all_b = []
+        for s in self.seqs:
+            f, p = self.forward(s)
+            all_f.append(f)
+            all_p.append(p)
+            all_b.append(self.backward(s))
+        all_f = np.array(all_f)
+        all_p = np.array(all_p)
+        all_b = np.array(all_b)
+
+        a = np.zeros_like(self.tp)
+        for k in range(10):
+            for l in range(10):
+                for j, s in enumerate(self.seqs):
+                    for i, char in enumerate(s):
+                        a[k, l] += (1.0 / all_p[j]) * all_f[j, k, i] * self.tp[k, l] * \
+                                   self.ep[l, to_index[char]] * all_b[j, l, i+1]
+        # TODO: fix issue in here (for now, commented it out since the emission probabilities are known in this problem)
+        e = np.zeros_like(self.ep)
+        for k in range(8):
+            for beta in range(4):
+                for j, s in enumerate(self.seqs):
+                    for i, char in enumerate(s):
+                        if to_index[char] == beta:
+                            e[k, beta] += (1.0 / all_p[j]) * all_f[j, k, i+1] * all_b[j, k, i+1]
+
+        ntp = np.sum(a, axis=1)[:, np.newaxis]
+        nep = np.sum(e, axis=1)[:, np.newaxis]
+        self.tp = np.divide(a, ntp, out=self.tp, where=ntp != 0)
+        self.ep = np.divide(e, nep, out=self.ep, where=nep != 0)
 
     def forward(self, seq: str) -> (ndarray, float):
         n = len(seq)
-        f = np.zeros((10, n+1))
+        f = np.zeros((10, n + 1))
         f[to_index['S'], 0] = 1.0
         for i, char in enumerate(seq):
             for state in range(10):
-                f[state, i+1] = self.ep[state, to_index[char]] * np.sum(self.tp[:, state] * f[:, i])
+                f[state, i + 1] = self.ep[state, to_index[char]] * np.sum(self.tp[:, state] * f[:, i])
         p = np.sum(self.tp[:, to_index['E']] * f[:, n])
         return f, p
 
     def backward(self, seq: str) -> ndarray:
         n = len(seq)
-        b = np.zeros((10, n+1))
+        b = np.zeros((10, n + 1))
         b[:, n] = self.tp[:, to_index['E']]
-        for i, char in enumerate(reversed(seq)):
+        for i, char in enumerate(reversed(seq[1:])):
             for state in range(10):
                 b[state, n-i-1] = np.sum(b[:, n-i] * self.ep[:, to_index[char]] * self.tp[state, :])
+        b[to_index['S'], 0] = np.sum(b[:, 1] * self.ep[:, to_index[seq[0]]] * self.tp[to_index['S'], :])
         return b
-
-    def update(self):
-        pass
 
 
 if __name__ == '__main__':
     # TODO: remove seed when testing is over
     np.random.seed(1)
-    seq = 'GGAACCAAGAACGAGGGGCAAGTGGGAGGAGGTGGTCACCTGGAGGGTGT' \
-          'GGACCAGTGGTACACAGGTTAGGAGAGGGGGAAGGGCAGAGTTTACATTG'
-    bw = BaumWelch(seq)
-    f, p = bw.forward(seq)
-    b = bw.backward(seq)
-    pass
+    # seqs = ['GGAACCAAGAACGAGGGGCAAGTGGGAGGAGGTGGTCACCTGGAGGGTGTGGACCAGTGGTACACAGGTTAGGAGAGGGGGAAGGGCAGAGTTTACATTG',
+    #        'CACTGGTGACTGTGGGCTGCATGGTGAAGGGGGGCAAAGCAGTGACAGCGGGAGTGAGCCCCTCTCAAAAACTGATGCCAACTACGCAGGACAGAGAGGG']
+    seqs = ['ACGTACGT',
+            'ACGTACGT']
+    bw = BaumWelch(seqs)
+    bw.run()
+    print(bw.tp)
+    print(bw.ep)
