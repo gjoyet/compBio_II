7e997ea46bac0eddb927b1035dda826ab2894d61
Guillaume Joyet <guillaume.joyet@unibas.ch>
2022-04-13 14:53:09 +0200

    Baum Welch seems to be working, although it is not tested yet. Rename BaumWelch to BaumWelchUnwrapped. New class BaumWelch implements the algorithm for a 4-state model.

--- src/detector/baum_welch_unwrapped.py
+++ src/detector/baum_welch_unwrapped.py
@@ -1,20 +1,24 @@
 import numpy as np
 from numpy import ndarray
 
+to_index = {'A': 0, 'C': 1, 'G': 2, 'T': 3,
+            'A+': 0, 'C+': 1, 'G+': 2, 'T+': 3,
+            'A-': 4, 'C-': 5, 'G-': 6, 'T-': 7,
+            'S': 8}
+to_state = {0: 'A+', 1: 'C+', 2: 'G+', 3: 'T+',
+            4: 'A-', 5: 'C-', 6: 'G-', 7: 'T-'}
 
-to_index = {'A': 0, 'C': 1, 'G': 2, 'T': 3}
-to_state = {0: 'A', 1: 'C', 2: 'G', 3: 'T'}
 
+class BaumWelchUnwrapped:
 
-class BaumWelch:
-
-    def __init__(self, seqs: list[str], tol=10 ** -8):
+    def __init__(self, seqs: list[str], tol=10**-8):
         # transition probabilities from row to column
-        self.tp = np.array([[0.9, 0.1], [0.1, 0.9]])
-        self.end_p = np.ones((2,))
-        self.start_p = np.array([0.5, 0.5])
+        self.tp = np.random.randn(9, 9) * 0.01 + 1.0 / 8.0
+        self.tp[:, to_index['S']] = np.zeros((9,))
+        self.tp /= np.sum(self.tp, axis=1)[:, np.newaxis]
+        self.tp_to_end = np.ones((9, ))
         # emission probabilities
-        self.ep = np.array([[0.2, 0.3, 0.3, 0.2], [0.25, 0.25, 0.25, 0.25]])
+        self.ep = np.concatenate((np.eye(4), np.eye(4), np.zeros((1, 4))))
         # sequences
         self.seqs = seqs
         self.tol = tol
@@ -43,49 +47,44 @@ class BaumWelch:
         all_b = np.array(all_b)
 
         a = np.zeros_like(self.tp)
-        for k in range(2):
-            for l in range(2):
+        for k in range(9):
+            for l in range(9):
                 for j, s in enumerate(self.seqs):
-                    for i, char in enumerate(s[:-1]):
+                    for i, char in enumerate(s):
                         a[k, l] += (1.0 / all_p[j]) * all_f[j, k, i] * self.tp[k, l] * \
                                    self.ep[l, to_index[char]] * all_b[j, l, i+1]
         e = np.zeros_like(self.ep)
-        for k in range(2):
+        for k in range(8):
             for beta in range(4):
                 for j, s in enumerate(self.seqs):
-                    for i, char in enumerate(s[:-1]):
+                    for i, char in enumerate(s):
                         if to_index[char] == beta:
                             e[k, beta] += (1.0 / all_p[j]) * all_f[j, k, i+1] * all_b[j, k, i+1]
-        s = np.zeros_like(self.start_p)
-        for k in range(2):
-            for j, s in enumerate(self.seqs):
-                s = all_b[j, :, 0] * self.ep[:, to_index[s[0]]] * self.start_p
 
         ntp = np.sum(a, axis=1)[:, np.newaxis]
         nep = np.sum(e, axis=1)[:, np.newaxis]
-        nsp = np.sum(s)
         self.tp = np.divide(a, ntp, out=self.tp, where=ntp != 0)
         self.ep = np.divide(e, nep, out=self.ep, where=nep != 0)
-        self.start_p = np.divide(s, nsp, out=self.start_p, where=nsp != 0)
         return np.sum(all_p)
 
     def forward(self, seq: str) -> (ndarray, float):
         n = len(seq)
-        f = np.zeros((2, n))
-        f[:, 0] = self.start_p * self.ep[:, to_index[seq[0]]]
-        for i, char in enumerate(seq[1:]):
-            for state in range(2):
+        f = np.zeros((9, n + 1))
+        f[to_index['S'], 0] = 1.0
+        for i, char in enumerate(seq):
+            for state in range(9):
                 f[state, i + 1] = self.ep[state, to_index[char]] * np.sum(self.tp[:, state] * f[:, i])
-        p = np.sum(self.end_p * f[:, -1])
+        p = np.sum(self.tp_to_end * f[:, n])
         return f, p
 
     def backward(self, seq: str) -> ndarray:
         n = len(seq)
-        b = np.zeros((2, n))
-        b[:, -1] = self.end_p
+        b = np.zeros((9, n + 1))
+        b[:, n] = self.tp_to_end
         for i, char in enumerate(reversed(seq[1:])):
-            for state in range(2):
-                b[state, -(i+2)] = np.sum(b[:, -(i+1)] * self.ep[:, to_index[char]] * self.tp[state, :])
+            for state in range(9):
+                b[state, n-i-1] = np.sum(b[:, n-i] * self.ep[:, to_index[char]] * self.tp[state, :])
+        b[to_index['S'], 0] = np.sum(b[:, 1] * self.ep[:, to_index[seq[0]]] * self.tp[to_index['S'], :])
         return b
 
 
@@ -96,7 +95,7 @@ if __name__ == '__main__':
             'CACTGGTGACTGTGGGCTGCATGGTGAAGGGGGGCAAAGCAGTGACAGCGGGAGTGAGCCCCTCTCAAAAACTGATGCCAACTACGCAGGACAGAGAGGG']
     # seqs = ['ACGTACGT',
     #         'CCAGTAGT']
-    bw = BaumWelch(seqs)
+    bw = BaumWelchUnwrapped(seqs)
     c = bw.run()
     np.set_printoptions(precision=3)
     print('\nIterations until convergence with tolerance {}: {}\n'.format(bw.tol, c))
@@ -105,6 +104,3 @@ if __name__ == '__main__':
     print('')
     print('Emission probabilities:')
     print(bw.ep)
-    print('')
-    print('Probabilities for initial state:')
-    print(bw.start_p)
