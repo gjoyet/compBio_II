dfd35b29755da84c0a3e449b89026a3468ba1e92
gjoyet <guillaume.joyet@unibas.ch>
2022-05-26 12:12:58 +0200

    Redo baum_welch.py, add baum_welch_scaled.py

--- src/detector/baum_welch.py
+++ src/detector/baum_welch.py
@@ -3,108 +3,102 @@ from numpy import ndarray
 
 
 to_index = {'A': 0, 'C': 1, 'G': 2, 'T': 3}
-to_state = {0: 'A', 1: 'C', 2: 'G', 3: 'T'}
 
 
 class BaumWelch:
 
-    def __init__(self, seqs: list[str], tol=10 ** -8):
-        # transition probabilities from row to column
-        self.tp = np.array([[0.9, 0.1], [0.1, 0.9]])
-        self.end_p = np.ones((2,))
-        self.start_p = np.array([0.5, 0.5])
-        # emission probabilities
-        self.ep = np.array([[0.2, 0.3, 0.3, 0.2], [0.25, 0.25, 0.25, 0.25]])
-        # sequences
+    def __init__(self, seqs: list[str], tol=1e-60):
+        self.tp = np.array([[0.85, 0.15, 0], [0.15, 0.85, 0], [0.5, 0.5, 0]])
+        self.n_states = self.tp.shape[0]
+        p = np.random.normal(0.25, 0.01, (2, 4))
+        self.ep = np.concatenate((p / np.sum(p, axis=1)[:, np.newaxis], np.zeros((1, 4))))
         self.seqs = seqs
         self.tol = tol
+        pass
 
-    def run(self):
+    def run(self, min_iter=25) -> int:
+        counts = 0
         p = 1.0
         p_next = 0.0
-        counts = 0
-        while abs(p - p_next) > self.tol or counts <= 100:
+        while abs(p_next - p) > self.tol or counts < min_iter:
             counts += 1
             p = p_next
             p_next = self.iterate()
+            if counts % 10 == 0:
+                print(p_next)
         return counts
 
-    def iterate(self):
-        all_f = []
-        all_p = []
-        all_b = []
-        for s in self.seqs:
-            f, p = self.forward(s)
-            all_f.append(f)
-            all_p.append(p)
-            all_b.append(self.backward(s))
-        all_f = np.array(all_f)
-        all_p = np.array(all_p)
-        all_b = np.array(all_b)
+    def iterate(self) -> float:
+        A = np.zeros_like(self.tp)
+        E = np.zeros_like(self.ep)
+        ptot = 0
 
-        a = np.zeros_like(self.tp)
-        for k in range(2):
-            for l in range(2):
-                for j, s in enumerate(self.seqs):
-                    for i, char in enumerate(s[:-1]):
-                        a[k, l] += (1.0 / all_p[j]) * all_f[j, k, i] * self.tp[k, l] * \
-                                   self.ep[l, to_index[char]] * all_b[j, l, i+1]
-        e = np.zeros_like(self.ep)
-        for k in range(2):
-            for beta in range(4):
-                for j, s in enumerate(self.seqs):
-                    for i, char in enumerate(s[:-1]):
-                        if to_index[char] == beta:
-                            e[k, beta] += (1.0 / all_p[j]) * all_f[j, k, i+1] * all_b[j, k, i+1]
-        s = np.zeros_like(self.start_p)
-        for k in range(2):
-            for j, s in enumerate(self.seqs):
-                s = all_b[j, :, 0] * self.ep[:, to_index[s[0]]] * self.start_p
+        for seq in seqs:
+            f, pf = self.forward(seq)
+            b, pb = self.backward(seq)
+            assert np.isclose(pf, pb, rtol=1e-10, atol=0)
+            ptot += pf
 
-        ntp = np.sum(a, axis=1)[:, np.newaxis]
-        nep = np.sum(e, axis=1)[:, np.newaxis]
-        nsp = np.sum(s)
-        self.tp = np.divide(a, ntp, out=self.tp, where=ntp != 0)
-        self.ep = np.divide(e, nep, out=self.ep, where=nep != 0)
-        self.start_p = np.divide(s, nsp, out=self.start_p, where=nsp != 0)
-        return np.sum(all_p)
+            for k in range(self.n_states):
+                for l in range(self.n_states):
+                    for i, char in enumerate(seq):
+                        A[k, l] += f[k, i] * self.tp[k, l] * self.ep[l, to_index[char]] * b[l, i+1] / pf
+
+            for k in range(self.n_states):
+                for i, char in enumerate(seq):
+                    E[k, to_index[char]] += f[k, i+1] * b[k, i+1] / pf
+
+        ntp = np.sum(A, axis=1)[:, np.newaxis]
+        nep = np.sum(E, axis=1)[:, np.newaxis]
+        self.tp = np.divide(A, ntp, out=self.tp, where=ntp != 0)
+        self.ep = np.divide(E, nep, out=self.ep, where=nep != 0)
+
+        return ptot
 
     def forward(self, seq: str) -> (ndarray, float):
-        n = len(seq)
-        f = np.zeros((2, n))
-        f[:, 0] = self.start_p * self.ep[:, to_index[seq[0]]]
-        for i, char in enumerate(seq[1:]):
-            for state in range(2):
-                f[state, i + 1] = self.ep[state, to_index[char]] * np.sum(self.tp[:, state] * f[:, i])
-        p = np.sum(self.end_p * f[:, -1])
+        L = len(seq)
+        f = np.zeros((self.n_states, L + 1))
+        f[-1, 0] = 1.0
+        for i, char in enumerate(seq):
+            for k in range(self.n_states-1):
+                f[k, i+1] = self.ep[k, to_index[char]] * np.inner(f[:, i], self.tp[:, k])
+
+        p = np.sum(f[:, -1])
         return f, p
 
-    def backward(self, seq: str) -> ndarray:
-        n = len(seq)
-        b = np.zeros((2, n))
-        b[:, -1] = self.end_p
-        for i, char in enumerate(reversed(seq[1:])):
-            for state in range(2):
-                b[state, -(i+2)] = np.sum(b[:, -(i+1)] * self.ep[:, to_index[char]] * self.tp[state, :])
-        return b
+    def backward(self, seq: str) -> (ndarray, float):
+        L = len(seq)
+        b = np.zeros((self.n_states, L + 1))
+        b[:-1, -1] = 1.0
+        for i, char in zip(range(L-1, 0, -1), reversed(seq[1:])):
+            for k in range(self.n_states-1):
+                b[k, i] = np.sum(b[:, i+1] * self.ep[:, to_index[char]] * self.tp[k, :])
+
+        b[-1, 0] = np.sum(b[:, 1] * self.ep[:, to_index[seq[0]]] * self.tp[-1, :])
+        p = b[-1, 0]
+        return b, p
+
+
+def read_seqs(filename, condition_nbr=1):
+    seqs = []
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+        for i, l in enumerate(lines):
+            if 'Condition_{}\n'.format(condition_nbr) in l:
+                seqs.append(lines[i+1].replace('\n', ''))
+
+    return seqs
 
 
 if __name__ == '__main__':
-    # TODO: remove seed when testing is over
-    np.random.seed(1)
-    seqs = ['GGAACCAAGAACGAGGGGCAAGTGGGAGGAGGTGGTCACCTGGAGGGTGTGGACCAGTGGTACACAGGTTAGGAGAGGGGGAAGGGCAGAGTTTACATTG',
-            'CACTGGTGACTGTGGGCTGCATGGTGAAGGGGGGCAAAGCAGTGACAGCGGGAGTGAGCCCCTCTCAAAAACTGATGCCAACTACGCAGGACAGAGAGGG']
-    # seqs = ['ACGTACGT',
-    #         'CCAGTAGT']
+    seqs = read_seqs('../../SequencesCpG.txt', 1)
     bw = BaumWelch(seqs)
-    c = bw.run()
+    its = bw.run()
     np.set_printoptions(precision=3)
-    print('\nIterations until convergence with tolerance {}: {}\n'.format(bw.tol, c))
+    print('\nIterations until convergence with tolerance {}: {}\n'.format(bw.tol, its))
     print('Transition matrix:')
     print(bw.tp)
     print('')
     print('Emission probabilities:')
     print(bw.ep)
     print('')
-    print('Probabilities for initial state:')
-    print(bw.start_p)
---
+++ src/detector/baum_welch_scaled.py
@@ -0,0 +1,112 @@
+import numpy as np
+from numpy import ndarray
+
+
+to_index = {'A': 0, 'C': 1, 'G': 2, 'T': 3}
+
+
+class BaumWelch:
+
+    def __init__(self, seqs: list[str], tol=1e-60):
+        self.tp = np.array([[0.85, 0.15, 0], [0.15, 0.85, 0], [0.5, 0.5, 0]])
+        self.n_states = self.tp.shape[0]
+        p = np.random.normal(0.25, 0.01, (2, 4))
+        self.ep = np.concatenate((p / np.sum(p, axis=1)[:, np.newaxis], np.zeros((1, 4))))
+        self.seqs = seqs
+        self.tol = tol
+        pass
+
+    def run(self, min_iter=25) -> int:
+        counts = 0
+        p = 1.0
+        p_next = 0.0
+        while abs(p_next - p) > self.tol or counts < min_iter:
+            counts += 1
+            p = p_next
+            p_next = self.iterate()
+            if counts % 10 == 0:
+                print(p_next)
+        return counts
+
+    def iterate(self) -> float:
+        A = np.zeros_like(self.tp)
+        E = np.zeros_like(self.ep)
+        ptot = 0
+
+        for seq in seqs:
+            f, pf = self.forward(seq)
+            b, pb = self.backward(seq)
+            assert np.isclose(pf, pb, rtol=1e-10, atol=0)
+            ptot += pf
+
+            for k in range(self.n_states):
+                for l in range(self.n_states):
+                    for i, char in enumerate(seq):
+                        A[k, l] += f[k, i] * self.tp[k, l] * self.ep[l, to_index[char]] * b[l, i+1] / pf
+
+            for k in range(self.n_states):
+                for i, char in enumerate(seq):
+                    E[k, to_index[char]] += f[k, i+1] * b[k, i+1] / pf
+
+        ntp = np.sum(A, axis=1)[:, np.newaxis]
+        nep = np.sum(E, axis=1)[:, np.newaxis]
+        self.tp = np.divide(A, ntp, out=self.tp, where=ntp != 0)
+        self.ep = np.divide(E, nep, out=self.ep, where=nep != 0)
+
+        return ptot
+
+    def forward(self, seq: str) -> (ndarray, float):
+        L = len(seq)
+        f = np.zeros((self.n_states, L+1))
+        f[-1, 0] = 1.0
+        G = np.zeros(L)
+        for i, char in enumerate(seq):
+            for k in range(self.n_states-1):
+                f[k, i+1] = self.ep[k, to_index[char]] * np.inner(f[:, i], self.tp[:, k])
+            G[i] = np.sum(f[:, i+1])
+            f[:, i+1] /= G[i]
+
+        p = np.prod(G)
+        return f, p
+
+    def backward(self, seq: str) -> (ndarray, float):
+        L = len(seq)
+        b = np.zeros((self.n_states, L+1))
+        b[:-1, -1] = 1.0
+        H = np.zeros(L)
+        for i, char in zip(range(L-1, 0, -1), reversed(seq[1:])):
+            for k in range(self.n_states-1):
+                b[k, i] = np.sum(b[:, i+1] * self.ep[:, to_index[char]] * self.tp[k, :])
+            H[i] = np.sum(b[:, i])
+            b[:, i] /= H[i]
+
+        b[-1, 0] = np.sum(b[:, 1] * self.ep[:, to_index[seq[0]]] * self.tp[-1, :])
+        H[0] = b[-1, 0]
+        b[:, 0] /= H[0]
+        p = np.prod(H)
+        return b, p
+
+
+def read_seqs(filename, condition_nbr=1):
+    seqs = []
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+        for i, l in enumerate(lines):
+            if 'Condition_{}\n'.format(condition_nbr) in l:
+                seqs.append(lines[i+1].replace('\n', ''))
+
+    return seqs
+
+
+if __name__ == '__main__':
+    seqs = read_seqs('../../SequencesCpG.txt', 1)
+    bw = BaumWelch(seqs)
+    its = bw.run()
+    np.set_printoptions(precision=3)
+    print('\nIterations until convergence with tolerance {}: {}\n'.format(bw.tol, its))
+    print('Transition matrix:')
+    print(bw.tp)
+    print('')
+    print('Emission probabilities:')
+    print(bw.ep)
+    print('')
